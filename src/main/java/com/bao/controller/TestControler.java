c15a34d42c1c4ab516f41653a36192f4febfdbce
manish-myteaching <manishverma.java@gmail.com>
2024-11-27 15:29:05 +0530

    Dev1 TestController

--- src/main/java/com/bao/controller/TestControler.java
+++ src/main/java/com/bao/controller/TestControler.java
@@ -3,9 +3,13 @@ package com.bao.controller;
 public class TestControler {
 
 	public static void main() {
-       System.out.println("hello");;
-       System.out.println("hi");
-       System.out.println("hi");
+		System.out.println("hello");
+		System.out.println("hi");
+		System.out.println("hi");
+	}
+
+	void n() {
+		System.out.println("n");
 	}
 
 }
