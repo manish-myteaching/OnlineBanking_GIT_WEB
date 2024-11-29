5fbad7f30997c5309273c2a391d9812b4e3d308d
manish2802 <manishverma.java@gmail.com>
2024-11-29 13:41:00 +0530

    Revert "Upi update"

--- src/main/java/com/bao/controller/TestControler.java
+++ src/main/java/com/bao/controller/TestControler.java
@@ -3,7 +3,7 @@ package com.bao.controller;
 public class TestControler {
 
 	public static void main() {
-		System.out.println("hello");;
+		System.out.println("hello");
 		System.out.println("hi");
 		System.out.println("hi");;
 		n();
--- src/main/java/com/bao/controller/UPIPaymentController.java
+++ src/main/java/com/bao/controller/UPIPaymentController.java
@@ -1,14 +1,9 @@
 package com.bao.controller;
 
 public class UPIPaymentController {
-
+	
 	public void getUPIDetails() {
-
-	}
-
-	public void upiUpdate() {
-		System.out.println("hi");
-
+		
 	}
 
 }
