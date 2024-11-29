e3fe1a276decfbec096c24b0458dce2a33f9e827
manish2802 <manishverma.java@gmail.com>
2024-11-29 10:28:31 +0530

    BOA4545 : Added Changes for UPI

--- src/main/java/com/bao/controller/UPIPaymentController.java
+++ src/main/java/com/bao/controller/UPIPaymentController.java
@@ -1,9 +1,14 @@
 package com.bao.controller;
 
 public class UPIPaymentController {
-	
+
 	public void getUPIDetails() {
-		
+
+	}
+
+	public void upiUpdate() {
+		System.out.println("hi");
+
 	}
 
 }
