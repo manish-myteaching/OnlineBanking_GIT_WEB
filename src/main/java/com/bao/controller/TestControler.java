8e696fda4e596c9818d9252aef53978d4c70422b
manish-myteaching <manishverma.myteaching@gmail.com>
2024-12-01 02:30:08 +0530

    gg

--- src/main/java/com/bao/controller/TestControler.java
+++ src/main/java/com/bao/controller/TestControler.java
@@ -3,8 +3,8 @@ package com.bao.controller;
 public class TestControler {
 
 	public static void main() {
-		System.out.println("hello");
-		System.out.println("hi...");
+		System.out.println("hello");;
+		System.out.println("hi...");;
 		System.out.println("hi");;
 		n();
 	}
