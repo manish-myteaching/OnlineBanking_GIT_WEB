69ab3cf45f40ab871d63c78743ba76d8524d527b
manish2802 <manishverma.java@gmail.com>
2024-12-02 15:37:08 +0530

    Added Changes for transfer

--- src/main/java/com/bao/controller/TransferController.java
+++ src/main/java/com/bao/controller/TransferController.java
@@ -3,5 +3,6 @@ package com.bao.controller;
 public class TransferController {
      public void transferAmmount() {
     	 System.out.println("transferAmmount");
+    	 System.out.println("accountholder");
      }
 }
