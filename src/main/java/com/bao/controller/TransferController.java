4b49391acec42f35c23ed73a1f6419c8687e52d5
manish2802 <manishverma.java@gmail.com>
2024-12-02 16:01:56 +0530

    Added Changed transfer ammount

--- src/main/java/com/bao/controller/TransferController.java
+++ src/main/java/com/bao/controller/TransferController.java
@@ -3,7 +3,6 @@ package com.bao.controller;
 public class TransferController {
      public void transferAmmount() {
     	 System.out.println("transferAmmount");
-    
-  
+    	 System.out.println("ifsc");
      }
 }
